10346099e6f0d2096738a47aeeba0e4a727a91c8
Spring -Git <vaahne@yahoo.com>
2025-04-01 14:49:51 -0600

    Due Date

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,13 @@ const add = document.getElementById("add");
 const todoContainerUl = document.querySelector("#todo");
 const h1 = document.querySelector("h1");
 const taskArea = document.querySelector(".taskArea");
-// localStorage.setItem("tasks",JSON.stringify([]));
+const task = document.getElementById("task");
+const dueDate = document.getElementById("dueDate");
+dueDate.placeholder = "Enter due date";
+
+//  localStorage.removeItem("tasks");
+
+// to get the existing tasks from localstorage if any.
 let localStorageTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 h1.classList.add("centerText");
@@ -17,22 +23,27 @@ form.addEventListener('submit', addList);
 
 fromLocalStorage();
 
-function addTemplate(todo,...flag) {
+function addTemplate(todo,due,...flag) {
 
+    // let due = dueDate.value;
     const template = document.querySelector("template");
     const newList = template.content.cloneNode(true);
 
     let todoList = newList.querySelector(".todoList");
 
+    const spanDiv = document.createElement("div");
+    spanDiv.classList.add("spanFlex");
+
     const span = document.createElement("span");
     span.textContent = todo;
-
-    // const textField = document.createElement("input");
-    // textField.type = "text";
-    // textField.value = todo;
-    // textField.style.border = 0;
-    // textField.readOnly = true;
+    span.setAttribute("title",todo);
     
+    const spanDue = document.createElement("span");
+    spanDue.textContent = due;
+    
+    spanDiv.appendChild(span);
+    spanDiv.appendChild(spanDue);
+
     const checkBox = document.createElement("input");
     checkBox.type = "checkbox";    
     const editBtn = document.createElement("button");
@@ -40,62 +51,91 @@ function addTemplate(todo,...flag) {
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
 
-    todoList.appendChild(span);
+    todoList.appendChild(spanDiv);
     todoList.appendChild(editBtn);
     todoList.appendChild(deleteBtn);
     todoList.appendChild(checkBox);
 
     todoContainerUl.appendChild(todoList);
-    
-    if(flag.length == 0){ // checks if it is coming from localStorage
-        localStorageTasks.push(todo);
+    // checks if it is coming from localStorage
+    if(flag.length == 0){ 
+        localStorageTasks.push(`${todo}:${due}`);
         localStorage.setItem("tasks",JSON.stringify(localStorageTasks));
     }
 
+    // when edit is clicked
     editBtn.addEventListener('click', (e) => {
         e.preventDefault();
         let newContent = prompt("Enter the todo list to be updated");
-        if(newContent != ""){
-            let index = localStorageTasks.indexOf(span.textContent);
-            span.textContent = newContent;
+        if(newContent != "" && newContent != null){
+            let task = span.textContent;            
+            // Editing the corresponding value in local storage
+            for(let i = 0;i<localStorageTasks.length;i++){
+                if(localStorageTasks[i].split(":")[0] == task){
+                    localStorageTasks[i] = newContent+":"+localStorageTasks[i].split(":")[1];
+                    break;
+                }
+            }
 
-            localStorageTasks[index] = newContent;
+            span.textContent = newContent;
+            //localStorageTasks[index] = newContent;
             localStorage.setItem("tasks",JSON.stringify(localStorageTasks));
-        }
+        }else
+            alert("New edited task cannot be empty");
     });
 
     deleteBtn.addEventListener('click', (e) => {
         e.preventDefault();
         let yesOrNo = confirm("Do you want to delete the task?");
         if (yesOrNo) {
-            let task = e.target.parentNode.children[0].textContent;
-            let index = localStorageTasks.indexOf(task);
-            if(index != -1){
-                localStorageTasks.splice(index,1);
-                localStorage.setItem("tasks",JSON.stringify(localStorageTasks));
-            }
+            let task = e.target.parentNode.children[0].children[0].textContent;
+            // checking the value in local storage and deleting it
+            for(let i = 0;i< localStorageTasks.length;i++){
+                if(localStorageTasks[i].split(":")[0] == task){
+                    localStorageTasks.splice(i,1);
+                    localStorage.setItem("tasks",JSON.stringify(localStorageTasks));
+                    break;
+                }
+            }            
             e.target.parentNode.remove();            
         }
     });
     checkBox.addEventListener('change',(e)=>{
         e.preventDefault();
-        if(checkBox.checked)
-            span.style.textDecoration = "line-through";
-        else
-            span.style.textDecoration = "none";
+        if(checkBox.checked){
+            spanDiv.style.textDecoration = "line-through";
+            spanDiv.style.textDecorationThickness = ".2rem";
+        }else
+            spanDiv.style.textDecoration = "none";
     });
 }
 
 function addList(e) {
     e.preventDefault();
-    const task = document.getElementById("task");
+    const regex = /^\d+$/;
+    
     task.classList.add("taskText");
-    addTemplate(task.value);
+    let taskVal = task.value;
+    let due = dueDate.value;
+    
+    if(Date.parse(due) < Date.parse(new Date())){
+        alert("Due date cannot be older than todays date");
+        dueDate.focus();
+        return;
+    }
+
+    if(regex.test(taskVal))
+        alert("Task cannot be numbers only");
+    else if(localStorageTasks.includes(taskVal))
+        alert("Task already present");
+    else
+        addTemplate(taskVal,due);
     form.reset();
 }
 
 function fromLocalStorage(){
-    localStorageTasks.forEach(element => {
-       addTemplate(element,true); 
+    localStorageTasks.forEach(element => {     // loop through all the existing data in local Storage  
+        let task = element.split(":");
+        addTemplate(task[0],task[1],true); 
     });
 }
